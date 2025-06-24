319b9d02ae8386987406b38ebcb23a6d5ebaecea
kukulo1 <kukulo1@Mail.ru>
2025-06-24 11:59:21 +0300

    add user entity

---
+++ src/main/java/ru/kukulo1/user_service/entity/User.java
@@ -0,0 +1,73 @@
+package ru.kukulo1.user_service.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+    @Size(min = 5, message = "Username must contain at least 5 characters")
+    @Setter
+    private String username;
+    @Size(min = 5, message = "Password must contain at least 5 characters")
+    @Setter
+    private String password;
+    @Transient
+    @Getter
+    @Setter
+    private String passwordConfirm;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @Getter
+    @Setter
+    private Set<Role> roles;
+
+    public User() {}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
