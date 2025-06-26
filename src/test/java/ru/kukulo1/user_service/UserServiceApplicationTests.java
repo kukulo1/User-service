ff73e299bd0aa9314ad6fde6b8f49914b1a823e2
kukulo1 <kukulo1@Mail.ru>
2025-06-27 01:35:39 +0300

    fix UserServiceApplicationTests so contextLoads test passes

--- src/test/java/ru/kukulo1/user_service/UserServiceApplicationTests.java
+++ src/test/java/ru/kukulo1/user_service/UserServiceApplicationTests.java
@@ -2,14 +2,30 @@ package ru.kukulo1.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@Testcontainers
 class UserServiceApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:16")
+			.withDatabaseName("testdb")
+			.withUsername("test")
+			.withPassword("test");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgresContainer::getUsername);
+		registry.add("spring.datasource.password", postgresContainer::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
 }
